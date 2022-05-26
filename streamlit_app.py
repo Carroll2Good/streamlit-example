5eac15335a955b80ffccacf850614ccd065269de
Carroll2Good <106291694+Carroll2Good@users.noreply.github.com>
2022-05-26 13:17:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,17 @@ import pandas as pd
 import streamlit as st
 
 """
-# Hell Love Bug!
+# Hello Love Bug!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+I Hope You Have A Lovely Sleep
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    total_points = st.slider("Number of times you want to have sex", 1, 5000, 2000)
+    num_turns = st.slider("Number of times we should have sex", 1, 100, 9)
 
     Point = namedtuple('Point', 'x y')
     data = []
